ecea0c309dda28a8b33a7e3e2f23e0dd6cc8d875
Chad Robinson <crobinson@medialantern.com>
2013-12-20 16:32:10 -0500

    Reactions are still a little weird.

--- apps/poll/plugins/reaction.js
+++ apps/poll/plugins/reaction.js
@@ -30,7 +30,20 @@ plugin.init = function() {
  * @echo_template
  */
 plugin.templates.head = '<div class="{plugin.class:head}"></div>';
-plugin.templates.bar = '<div class="{plugin.class:bar} result-bar"></div><div class="percentage"></div><div class="count"></div>';
+
+/**
+ * Width-based result bar and result text region.
+ *
+ * @echo_template
+ */
+plugin.templates.bar = '<div class="{plugin.class:bar} results"></div><div class="resultText results"></div>';
+
+/**
+ * Clearfix bar under the floated elements.
+ * TODO: Change to a real clearfix?
+ *
+ * @echo_template
+ */
 plugin.templates.clear = '<div style="clear: both"></div>';
 
 plugin.renderers.head = function(element) {
@@ -58,8 +71,9 @@ plugin.css =
 	'.{plugin.class} .{class:subwrapper} { margin: 0; }' +
     '.{plugin.class} .{class:container-root-thread} { padding: 0; }' +
 	'.{plugin.class} .{class:depth-1} { margin: 0; padding: 0; background-color: transparent; }' +
-	'.{plugin.class} .{class:children} .{class} { margin: 7px 0; background: #444; color: #fff; font-size: 13px; width: 49%; float: left; }' +
-	'.{plugin.class} .{class:children} .{class}:nth-child(odd) { margin-right: 2%; }' +
+	'.{plugin.class} .{class:children} .{class} { margin: 14px; background: #444; color: #fff; font-size: 16px; line-height: 40px; display: block; float: left; border: 2px solid #333; box-sizing: border-box; }' +
+	'.{plugin.class} .{class:children} .{class}:hover { background: #666; border: 2px solid DarkOrange; }' +
+
 
     // We move the header, and we don't show the inset even if it's there.
 	'.{plugin.class} .{class:children} .{class:text} .header,' +
@@ -69,12 +83,12 @@ plugin.css =
     '.{plugin.class} .{class:text} .question { width: 100%; padding: 7px 10px; line-height: 18px; font-size: 14px; text-transform: uppercase; background: #111; color: #fff; -moz-box-sizing: border-box; -webkit-box-sizing: border-box; box-sizing: border-box; }' +
 
 	'.{plugin.class} .{class:children} .{class} { font-size: 16px; line-height: 40px; }' +
-	'.{plugin.class} .{class:children} .{class:text} a { color: #fff; text-decoration: none; text-transform: uppercase; font-weight: bold; display: block; padding: 0 10px; }' +
+	'.{plugin.class} .{class:children} .{class:text} a { color: #fff; text-decoration: none; text-transform: uppercase; font-weight: bold; display: inline-block; padding: 0 10px; text-align: center; }' +
     '.{plugin.class} .{class:data} .percentage { float: right; margin: 0 7px; }' +
     '.{plugin.class} .{class:data} .count { float: right; margin: 0 7px; }' +
 
     // Body/Result bar layering
-    '.{plugin.class} .{class:children} .{class:data} { position: relative; height: 40px; width: 100%; }' +
+    '.{plugin.class} .{class:children} .{class:data} { position: relative; height: 40px; width: 120px; }' +
 	'.{plugin.class} .{class:children} .{class:body} { position: absolute; z-index: 2; top: 0; left: 0; bottom: 0; right: 0; padding: 0; }' +
     '.{plugin.class} .{class:children} .{plugin.class:bar} { position: absolute; z-index: 1; top: 0; left: 0; bottom: 0; color: #fff; line-height: 40px; font-size: 18px; background: #417DC1; border-right: 1px solid #ccc; }' +
 
@@ -143,25 +157,9 @@ plugin.methods.processData = function() {
     $.map(stream.threads[0].children, function(item, i) {
         var $wrapper = item.config.get('target'),
             $bar = item.plugins.Reaction.view.get('bar'),
-            percentage = item.get('percentage') || 50,
-            html = '';
+            percentage = item.get('percentage') || 50;
 
-        // Also see if we have an inset image
-//		var $img = $('<div>' + item.get('data.object.content') + '</div>').find('.inset');
-//        if ($img.length > 0) {
-//            html += $img.wrapAll('<div></div>').parent().html();
-//            console.log(html);
-//        }
-
-        if (item.config.get('showPercent')) {
-            $wrapper.find('.percentage').html(Math.round(percentage) + '%');
-        }
-
-        if (item.config.get('showCount')) {
-            $wrapper.find('.count').html(item.get('votes'));
-        }
-
-        $bar.html(html);
+        $wrapper.find('.resultText').html(item.get('resultText'));
 
         // jQuery sets overflow:hidden during animations, and we're using
         // overflow to position the buttons.
@@ -180,7 +178,13 @@ plugin.methods.processData = function() {
     });
 };
 
-plugin.css = '';
+plugin.css =
+    // Show/hide the results elements. This is done separately from animation.
+    // TODO: Move the animation to CSS3 and just have the JS trigger it.
+    '.{plugin.class} .results { display: none; }' +
+
+    '.{plugin.class}.show-results-before .results { display: block; }' +
+    '.{plugin.class}.show-results-after.voted .results { display: block; }';
 
 Echo.Plugin.create(plugin);
 
